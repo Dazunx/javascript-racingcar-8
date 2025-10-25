64d1e564f35d5993f1696770a34d74697374c839
DajeongLee <dajeong@DajeongLeeui-MacBookPro.local>
2025-10-25 17:51:24 +0900

    feat(app): complete basic racing logic with validation and random movement

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
       const carNamesInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분): ");
-      const carNames = carNamesInput.split(",").map((name) => name.trim());
+      const carNames = carNamesInput.split(",");
+
+       carNames.forEach((name) => {
+        const trimmed = name.trim();
+        if (trimmed.length > 5 || trimmed.length === 0) {
+          throw new Error("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+        }
+      });
 
       const tryCountInput = await MissionUtils.Console.readLineAsync("시도할 횟수: ");
       const tryCount = Number(tryCountInput);
 
-      MissionUtils.Console.print(`입력된 차량: ${carNames.join(", ")}`);
-      MissionUtils.Console.print(`시도 횟수: ${tryCount}`);
+      if (isNaN(tryCount) || tryCount <= 0) {
+        throw new Error("[ERROR] 시도 횟수는 1 이상의 숫자여야 합니다.");
+      }
 
       for (let i = 0; i < tryCount; i++) {
             for (let j = 0; j < carNames.length; j++) {
