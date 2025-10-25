17810284683d4c3ce83c48ba582c1eba66614adb
DajeongLee <dajeong@DajeongLeeui-MacBookPro.local>
2025-10-25 17:41:06 +0900

    feat(app): add console input/output logic for car names and try count

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+      const carNamesInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분): ");
+      const carNames = carNamesInput.split(",").map((name) => name.trim());
+
+      const tryCountInput = await MissionUtils.Console.readLineAsync("시도할 횟수: ");
+      const tryCount = Number(tryCountInput);
+
+      MissionUtils.Console.print(`입력된 차량: ${carNames.join(", ")}`);
+      MissionUtils.Console.print(`시도 횟수: ${tryCount}`);
+  }
 }
 
 export default App;
