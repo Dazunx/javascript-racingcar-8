5df2dfb43d9a9a53b2f2d171231f968e426f0c0a
DajeongLee <dajeong@DajeongLeeui-MacBookPro.local>
2025-10-27 08:41:10 +0900

    refactor(App): handle runtime errors safely and remove unused index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import { startGame } from "./controllers/racingController.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    try {
-      await startGame();
-    } catch (error) {
-      if (process.env.JEST_WORKER_ID !== undefined) {
-        throw error;
-      }
-      MissionUtils.Console.print(error.message || "[ERROR]");
-    }
+    await startGame();
   }
 }
 
